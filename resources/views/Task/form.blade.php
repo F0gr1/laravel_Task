5f7ef884fb9e0d9f9ac64a9355f76363faabe472
F0gr1 <19aj009@ms.dendai.ac.jp>
2021-11-25 21:37:04 +0900

    a

--- resources/views/Task/form.blade.php
+++ resources/views/Task/form.blade.php
@@ -15,11 +15,18 @@
                             <label for="task">タスク名</label>
                             <input type="text" class="form-control" name="task" value="{{ $task->task }}">
                         </div>
-                        <div class="form-group">
-                            <label for='name'>作成者</label>
-                            <label class="form-control" name="User" > {{ $userName->name }}</label>
-                            <input type='hidden' class="form-control" name="User" value="{{ $userName->name }}"/> 
-                        </div>
+                        @if($target == 'store')
+                            <div class="form-group">
+                                <label for='name'>作成者</label>
+                                <label class="form-control" name="User" > {{ $userName->name }}</label>
+                                <input type='hidden' class="form-control" name="User" value="{{ $userName->name }}"/> 
+                            </div>
+                            @elseif($target == 'update')
+                            <div class="form-group">
+                                <label for='name'>作成者</label>
+                                <input type='type' class="form-control" name="User" value="{{ $userName->name }}"/> 
+                            </div>
+                            @endif
                         <button type="submit" class="btn btn-default">登録</button>
                         <a href="/home">戻る</a>
                     </form>
