a0c71fe9901f5e9858d466cf3a3600255ace36a1
F0gr1 <gami20010314@gmail.com>
2021-11-27 20:37:57 +0900

    Oauth

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-
+use Laravel\Socialite\Facades\Socialite;
 class LoginController extends Controller
 {
     /*
