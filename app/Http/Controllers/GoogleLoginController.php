3c31adebcb74129d9cd8118ee82796f9dcd4ea46
F0gr1 <19aj009@ms.dendai.ac.jp>
2021-11-27 19:50:14 +0900

    Oauth2

---
+++ app/Http/Controllers/GoogleLoginController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+class GoogleLoginController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+    public function redirectToGoogle()
+    {
+        // Google へのリダイレクト
+        return Socialite::driver('google')->redirect();
+    }
+    public function handleGoogleCallback()
+    {
+        // Google 認証後の処理
+        $gUser = Socialite::driver('google')->stateless()->user();
+        $user = User::where('email' , $gUser->email)->first();
+        if($user == null){
+            $user = $this -> createUserByGoogle($gUser);
+        }
+        \Auth::login($user, true);
+        return redirect('/book');       
+    dd($gUser); 
+    }
+    public function createUserByGoogle($gUser)
+    {
+        $user = User::create([
+            'name'     => $gUser->name,
+            'email'    => $gUser->email,
+            'password' => \Hash::make(uniqid()),
+        ]);
+        return $user;
+    }
+}
+
