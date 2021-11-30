02b6d9c7e729671fc7f567e4551c005c9770641b
F0gr1 <gami20010314@gmail.com>
2021-12-01 01:05:14 +0900

    create を追加

--- app/Http/Controllers/ShowTaskController.php
+++ app/Http/Controllers/ShowTaskController.php
@@ -9,8 +9,18 @@ use Illuminate\Support\Facades\Auth;
 class ShowTaskController extends Controller
 {
     public function add(){
+        $showTask = new ShowTask();
         $tasks = DB::table('tasks')->get();
         $users = DB::table('users')->get();
         return view('User/userAdd', compact('tasks' , 'users'));
+    }
+    public function store(Request $request)
+    {
+        $showTask = new ShowTask();
+        $showTask-> taskId = $taskid;
+        $showTask->userId =$userid;
+        $showTask -> save();
+
+        return redirect("/home");
     }
 }
