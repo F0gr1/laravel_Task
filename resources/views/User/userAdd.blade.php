052e09ce18cf7e2cbe94e428de2ad764934aade4
F0gr1 <gami20010314@icloud.com>
2021-12-01 01:45:20 +0900

    user id 取得手前

--- resources/views/User/userAdd.blade.php
+++ resources/views/User/userAdd.blade.php
@@ -14,14 +14,14 @@
                 <form action="/user/add/" method="post">
                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
                     <div class="form-group flex">
-                    <select name="name_id[]"   class='form-control'>
+                    <select name="userId"   class='form-control' value='userId'>
                         @foreach($users as $user)
-                            <option value="{{ $user->id }}">{{ $user->name }}</option>
+                            <option value="$user -> id"　name="user">{{ $user->name}}</option>
                         @endforeach
                     </select>
-                    <select name="job_id[]"  class='form-control'>
+                    <select name="job_id[]"  class='form-control' value='taskid'>
                         @foreach($tasks as $task )
-                            <option value="{{ $task->id }}">{{ $task->task }}</option>
+                            <option value="{{ $task->id }}" name="task">{{ $task->task }}</option>
                         @endforeach 
                     </select>
                         <button type="submit" class="btn btn-default">登録</button>
