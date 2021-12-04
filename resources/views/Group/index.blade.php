3aa1d80acdd807b05c93e0f649a4d2e24dedaa19
satoshi <kanimo3104@gmail.com>
2021-12-05 02:04:40 +0900

    Revert "骨組み２"
    This reverts commit 1b1ae5b4c1b1f54d190562867913a78157c1ad48.

--- resources/views/Group/index.blade.php
+++ resources/views/Group/index.blade.php
@@ -7,6 +7,7 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header">グループ</div>
+
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
@@ -14,6 +15,7 @@
                         </div>
                     @endif
                 </div>
+
                 <div class="card-body">
                     <div class="row">
                         <div class="col-md-10 col-md-offset-1">
@@ -23,20 +25,12 @@
                                     <th class="text-center">編集</th>
                                     <th class="text-center">削除</th>
                                 </tr>
-                            </table>
-                            @foreach($groups as $group)
-                            <tr>
-                                <th>{{$group->group_name}}</th>
-                                <th>編集</th>
-                                <td>
-                                    <form action="/home/group{{$group->$id}}" method="post">
-                                        <input type="hidden" name="_method" value="DELETE">
-                                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                        <button type="submit" class="btn btn-xs btn-danger" aria-label="Left Align"><span class="glyphicon glyphicon-trash"></span></button>
                                     </form>
-                                </td>
-                            </tr>
-                            @endforeach
+                                    </td>
+                                </tr>
+                            
+                            </table>
+                            
                         </div>
                     </div>
                 </div>
