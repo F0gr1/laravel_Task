e3664079bd5932890968d918049a83cda2cb45e9
ultramarine-kanaria <kanaria121031@gmail.com>
2021-12-05 03:43:55 +0900

    GroupsとUsersGroupsのSeederファイルの作成

---
+++ database/seeders/UsersGroupsTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersGroupsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users_groups')->truncate();
+
+        // 初期データ用意（列名をキーとする連想配列）
+        for ($i = 1; $i <= 10; $i++) {
+          $usersGroups = [
+                      [
+                          'user_id' => $i,
+                          'group_id' => $i,
+                      ]
+          ];
+          DB::table('users_groups')->insert($usersGroups);
+        }
+    }
+}
