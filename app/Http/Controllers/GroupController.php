ac668f9b93ab9abb70b65e24aa24b11c5a5e7276
ultramarine-kanaria <kanaria121031@gmail.com>
2021-12-07 17:47:41 +0900

    create,storeで所属メンバーの変更を可能にした

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -6,35 +6,35 @@ use Illuminate\Http\Request;
 use App\Models\Group;
 use App\Models\Project;
 use App\Models\Task;
+use App\Models\UserGroup;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 
 class GroupController extends Controller
 {
     public function index()
     {
-        // 自分のuserIdとgroup_leader_idが一致するレコードを渡す
         $userId = Auth::id();
-        $groups = Group::where('group_leader_id' , '=' , $userId)
+        $group = Group::where('group_leader_id' , '=' , $userId)
         ->join('users_groups', 'groups.id', '=', 'users_groups.group_id')
         ->get();
-        return view('Group/index', compact('groups'));
+        return view('Group/index', compact('group'));
     }
 
     public function edit($groupId)
     {
-        // 選択したグループのレコードとそのグループに所属するuserのレコードを渡す
         $group = Group::findOrFail($groupId);
-        $userId = GroupUser::where('group_id' , '=' , $groupId)->get();
+        $userId = UserGroup::where('group_id' , '=' , $groupId)->get();
         $user = User::find($userId);
         return view('Group/edit', compact('group' , 'user'));
     }
 
     public function create($id)
     {
-        // 空の$bookと自分のuserNameを渡す
         $group = new Group();
-        $userName = Auth::user();
+        $user = Auth::user();
+        $group->group_leader_id = $user->id;
         return view('Group/create', compact('group' , 'userName'));
     }
 
@@ -42,13 +42,27 @@ class GroupController extends Controller
         $group = Group::findOrFail($id);
         $group->group_name = $request->group_name;
         $group->save();
+        UserGroup::where('group_id', '=', $group->id)->delete();
+        foreach($request->userId as $userId){
+            $userGroup = new UserGroup();
+            $userGroup->user_id = $userId;
+            $userGroup->group_id = $group->id;
+            $userGroup->save();
+        }
         return redirect("Group/index");
     }
 
     public function store(Request $request){
         $group = new Group();
         $group->group_name = $request->group_name;
+        $group->group_leader_id = Auth::id();
         $group->save();
+        foreach($request->userId as $userId){
+            $userGroup = new UserGroup();
+            $userGroup->user_id = $userId;
+            $userGroup->group_id = $group->id;
+            $userGroup->save();
+        }
         return redirect("Group/index");
     }
 
