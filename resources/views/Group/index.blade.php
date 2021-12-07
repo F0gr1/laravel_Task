63edacae137dc0bdbb7dabe6700cc46b0d04f1bf
satoshi <kanimo3104@gmail.com>
2021-12-07 17:06:35 +0900

    削除ボタンの実装と、値の表示

--- resources/views/Group/index.blade.php
+++ resources/views/Group/index.blade.php
@@ -17,25 +17,28 @@
                 <div class="card-body">
                     <div class="row">
                         <div class="col-md-10 col-md-offset-1">
-                            <table class="table text-center">
-                                <tr>
-                                    <th class="text-center">グループ</th>
-                                    <th class="text-center">編集</th>
-                                    <th class="text-center">削除</th>
-                                </tr>
-                                @foreach($groups as $group)
+                            <table class="table text-out-center">
                             <tr>
-                                <th>{{$group->group_name}}</th>
-                                <th>編集</th>
-                                <td>
-                                    <form action="/home/group{{$group->$id}}" method="post">
+                                <th class="text-center">グループ</th>
+                                <th class="text-center">編集</th>
+                                <th class="text-center">削除</th>
+                            </tr>
+                            @foreach($groups as $group)
+                            <tr>
+                                <th class = "text-center">{{$group->group_name}}</th>
+                                <th class = "text-center">編集</th>
+                                <td class = "text-center">
+                                    <form action="/home/group/{{$group->id}}" method="post">
                                         <input type="hidden" name="_method" value="DELETE">
                                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                                         <button type="submit" class="btn btn-xs btn-danger" aria-label="Left Align"><span class="glyphicon glyphicon-trash"></span></button>
                                     </form>
                                 </td>
                             </tr>
-                            @endforeach                            
+                            @endforeach
+                            </table>                            
+                            <div>
+                                <a href="/home/group/create" class="btn btn-outline-success">新規作成</a></div>
                         </div>
                     </div>
                 </div>
