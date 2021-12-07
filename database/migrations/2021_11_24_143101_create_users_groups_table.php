0e4f373a10ed46f97911a105c044fdbcb6562841
ultramarine-kanaria <kanaria121031@gmail.com>
2021-12-07 17:49:36 +0900

    外部キーで更新削除時の挙動をcascadeに指定

--- database/migrations/2021_11_24_143101_create_users_groups_table.php
+++ database/migrations/2021_11_24_143101_create_users_groups_table.php
@@ -16,9 +16,9 @@ class CreateUsersGroupsTable extends Migration
         Schema::create('users_groups', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger("user_id");
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->unsignedBigInteger("group_id");
-            $table->foreign('group_id')->references('id')->on('groups');
+            $table->foreign('group_id')->references('id')->on('groups')->onDelete('cascade');
             $table->timestamps();
         });
     }
