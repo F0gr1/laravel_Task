3ce90beee2b7a7645a0ff356d18e7df80f31d41b
F0gr1 <gami20010314@icloud.com>
2021-12-08 18:32:29 +0900

    updateとstoreの長いリクエスト処理

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Project;
 use App\Models\Task;
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
 class ProjectController extends Controller
 {
@@ -22,41 +23,28 @@ class ProjectController extends Controller
     public function edit($id)
     {
         $Project = Project::findOrFail($id);
-        // 取得した値をビュー「book/edit」に渡す
-        return view('Project/edit', compact('Project'));
+        $users = User::get();
+        return view('Project/edit', compact('Project' , 'users'));
     }
     public function update(Request $request , $id)
     {
         $Projects = Project::findOrFail($id);
-        $Projects->project = $request->project;
-        $Projects->User = $request->User;
-        $Projects->memo = $request->memo;
-        $Projects->progress= $request->progress;
-        $Projects->start_date = $request->start_date;
-        $Projects->end_date = $request->end_date;
-        $Projects->save();
-
-        return redirect("/home");
+        $Projects->fill($request->all())->save();
+        return back();
     }
     public function create($id)
     {
-         // 空の$bookを渡す
+         // 空の$Projectを渡す
         $Project = new Project();
         $Task = Task::findOrFail($id);
-        return view('Project/create', compact('Project', 'Task'));
+        $users = User::get();
+        return view('Project/create', compact('Project', 'Task' , 'users'));
     }
 
     public function store(Request $request)
     {
         $Projects = new Project();
-        $Projects->project = $request->project;
-        $Projects->taskId = $request->taskId;
-        $Projects->memo = $request->memo;
-        $Projects->progress = $request->progress;
-        $Projects->User = $request->User;
-        $Projects->start_date = $request->start_date;
-        $Projects->end_date = $request->end_date;
-        $Projects->save();
+        $Projects->fill($request->all())->save();
         return redirect("/home");
     }
     public function delete($id)
