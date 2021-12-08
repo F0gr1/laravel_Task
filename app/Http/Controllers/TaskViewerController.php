3c05736a6e94fdae1aa24b133e9c5ed4aa7c36ec
F0gr1 <gami20010314@icloud.com>
2021-12-08 23:39:03 +0900

    TaskViewerのtypo変更

--- app/Http/Controllers/TaskViewerController.php
+++ app/Http/Controllers/TaskViewerController.php
@@ -5,20 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Task;
 use App\Models\User;
-use App\Models\taskviewer;
+use App\Models\TaskViewer;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 class TaskViewerController extends Controller
 {
     public function add(){
-        $viewer = new taskviewer();
+        $viewer = new TaskViewer();
         $tasks = Task::get();
         $users = User::get();
         return view('User/userAdd', compact('tasks' , 'users' , 'viewer'));
     }
     public function store(Request $request)
     {
-        $viewer = new taskviewer();
+        $viewer = new TaskViewer();
         $task = new Task();
         $viewer-> taskId = $request->taskId;
         $viewer-> userId = $request->userId;
