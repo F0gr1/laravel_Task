c63c54aa6c302d0106813cb2841ffaf1dcd4539a
F0gr1 <gami20010314@icloud.com>
2021-12-13 17:54:15 +0900

    groupが複数とユーザーが複数いた場合の処理;

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Task;
 use App\Models\Group;
 use App\Models\TaskViewer;
+use App\Models\UsersGroup;
 use Illuminate\Support\Facades\App; 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Pagination\Paginator;
@@ -70,14 +71,16 @@ class TaskController extends Controller
             $task->task = $request->task;
             $task->user = $request->user;
             $task->group_id = $groupId;
+            $users=UsersGroup::where('group_id', '=', $groupId)->get();
             $task->save();
+            foreach($users as $user){
+                $taskId = DB::table('tasks')->orderby('id' , 'desc')->first();
+                $viewer = new TaskViewer();
+                $viewer->taskId = $taskId->id;
+                $viewer->userId = $user->user_id;
+                $viewer->save();
+            }
         }
-        $taskId = DB::table('tasks')->orderby('id' , 'desc')->first();
-        $viewer = new TaskViewer();
-        $id = Auth::id();
-        $viewer->taskId = $taskId->id;
-        $viewer->userId = $id;
-        $viewer->save();
 
         return redirect("/home");
     }
