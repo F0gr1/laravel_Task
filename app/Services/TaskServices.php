6b4675f0d46307d7033ce91fe740c2055e3df8af
F0gr1 <gami20010314@gmail.com>
2022-03-02 04:54:01 +0900

    update　table定義とグループの取得

--- app/Services/TaskServices.php
+++ app/Services/TaskServices.php
@@ -4,6 +4,7 @@ namespace App\Services;
 use Illuminate\Http\Request;
 use App\Models\Task;
 use App\Models\Group;
+use App\Models\User;
 use App\Models\TaskViewer;
 use App\Models\UsersGroup;
 use App\Services\TaskServices;
@@ -34,7 +35,8 @@ class TaskServices
     }
     public function getGroup(){
         $user_id = Auth::id();
-        $groups = Group::find($user_id)->with('group')->get();
+        $groups = User::find($user_id)->group()->get();
+        
         return $groups;
     }
     public function taskUpdate(Request $request ,int $id){
@@ -46,7 +48,7 @@ class TaskServices
         }
     }
     public function getGroups(int $user_id){
-        $groups= Group::find($user_id)->with('group')->get();
+        $groups= User::find($user_id)->group()->get();
         return $groups;
     }
     public function taskStore( Request $request ){
