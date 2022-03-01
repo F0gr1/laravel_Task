df73963da523c545aca000e076468f9687e5995e
F0gr1 <gami20010314@gmail.com>
2022-03-02 04:47:11 +0900

    update　多 vs 多だったのでモデルの定義変更

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -8,12 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Group extends Model
 {
     use HasFactory;
-    public function user()
+   
+    public function  user()
     {
-        return $this->hasOne(User::class , 'id');
-    }
-    public function  group()
-    {
-        return $this->hasMany(UsersGroup::class ,'group_id');
+        return $this->belongsToMany(User::class ,'users_groups');
     }
 }
